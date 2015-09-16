526700a478c61b3f2c4e1ef9aac6bad2b2421c78
Cristian <cristianchaparroa@gmail.com>
2015-09-15 22:01:44 -0500

    Update a templates in www with gulp

---
+++ examples/workflow/nmp.dev.sh
@@ -0,0 +1,14 @@
+npm install -g gulp
+npm install -g bower 
+
+
+npm install --save-dev gulp
+npm install --save-dev gulp-connect
+npm install --save-dev connect-history-api-fallback
+npm install --save-dev gulp-jshint
+npm install --save-dev gulp-useref
+npm install --save-dev gulp-if
+npm install --save-dev gulp-uglify
+npm install --save-dev gulp-minify-css
+npm install --save-dev gulp-stylus
+npm install --save-dev nib
